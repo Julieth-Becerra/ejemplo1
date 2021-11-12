046176b7905e710e0d6985020df11bcc1c14dc27
Julieth-Becerra <julieth.becerra01@uptc.edu.co>
2021-11-12 08:24:08 -0500

    master

---
+++ cmake-build-debug/CMakeFiles/ejemplo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ejemplo.dir/main.cpp.obj"
+  "ejemplo.exe"
+  "ejemplo.exe.manifest"
+  "ejemplo.pdb"
+  "libejemplo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ejemplo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ejemplo.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ejemplo.
+# This may be replaced when dependencies are built.
